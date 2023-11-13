d2b033831407ce3677c7444ada71b79a65e75373
Piotr Zienowicz <115182798+piozien@users.noreply.github.com>
2023-11-13 09:51:38 +0100

    lab6

--- probnekolokwium/src/furniture/Table.java
+++ probnekolokwium/src/furniture/Table.java
@@ -2,12 +2,16 @@ package furniture;
 
 class Table {
     private double price;
+    public static Table instance;
 
     private Table(double price) {
         this.price = price;
     }
 
-    public double getPrice() {
-        return price;
+    public static Table getInstance(double price) {
+        if (instance == null) {
+            instance = new Table(price);
+        }
+        return instance;
     }
 }
--- probnekolokwium/src/furniture/TestTable.java
+++ probnekolokwium/src/furniture/TestTable.java
@@ -2,8 +2,9 @@ package furniture;
 
 public class TestTable {
     public static void main(String[] args) {
-        Table t1 = new Table(123.45);
-        System.out.println("Table price: " + t1.getPrice());
+        Table t1 = Table.getInstance(123.45);
+        System.out.println(t1);
+
     }
 }
 
